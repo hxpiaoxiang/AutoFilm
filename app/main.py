26ab2fc325dcf76a9d2d4a68f673b4e0d1754f8e
hxpiaoxiang <37627418+hxpiaoxiang@users.noreply.github.com>
2024-07-26 17:18:04 +0800

    Update main.py
    取消定时任务

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ from sys import path
 from os.path import dirname
 path.append(dirname(dirname(__file__)))
 
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.cron import CronTrigger
+# from apscheduler.schedulers.asyncio import AsyncIOScheduler
+# from apscheduler.triggers.cron import CronTrigger
 
 from app.core import settings
 from app.extensions import LOGO
@@ -18,35 +18,36 @@ if __name__ == "__main__":
     print(LOGO + str(settings.APP_VERSION).center(65, "="))
     print(f"AutoFilm {settings.APP_VERSION}启动中...")
 
-    scheduler = AsyncIOScheduler()
+    # scheduler = AsyncIOScheduler()
     
     if settings.AlistServerList:
-        logger.info("检测到Alist2Strm模块配置，正在添加至后台任务")
+        print("检测到Alist2Strm模块配置，正在执行")
         for server in settings.AlistServerList:
-            cron = server.get("cron")
-            if cron:
-                scheduler.add_job(Alist2Strm(**server).run,trigger=CronTrigger.from_crontab(cron))
-                print(f"{server["id"]}已被添加至后台任务")
-            else:
-                print(f"{server["id"]}未设置Cron")
+            # cron = server.get("cron")
+            # if cron:
+            #     scheduler.add_job(Alist2Strm(**server).run,trigger=CronTrigger.from_crontab(cron))
+            #     print(f"{server["id"]}已被添加至后台任务")
+            # else:
+            #     print(f"{server["id"]}未设置Cron")
+            Alist2Strm(**server)
     else:
         print("未检测到Alist2Strm模块配置")
 
-    if settings.Ani2AlistList:
-        print("检测到Ani2Alist模块配置，正在添加至后台任务")
-        for server in settings.Ani2AlistList:
-            cron = server.get("cron")
-            if cron:
-                scheduler.add_job(Ani2Alist(**server).run,trigger=CronTrigger.from_crontab(cron))
-                print(f"{server["id"]}已被添加至后台任务")
-            else:
-                print(f"{server["id"]}未设置Cron")
-    else:
-        print("未检测到Ani2Alist模块配置")
+    # if settings.Ani2AlistList:
+    #     print("检测到Ani2Alist模块配置，正在添加至后台任务")
+    #     for server in settings.Ani2AlistList:
+    #         cron = server.get("cron")
+    #         if cron:
+    #             scheduler.add_job(Ani2Alist(**server).run,trigger=CronTrigger.from_crontab(cron))
+    #             print(f"{server["id"]}已被添加至后台任务")
+    #         else:
+    #             print(f"{server["id"]}未设置Cron")
+    # else:
+    #     print("未检测到Ani2Alist模块配置")
 
-    scheduler.start()
+    # scheduler.start()
 
-    try:
-        get_event_loop().run_forever()
-    except (KeyboardInterrupt, SystemExit):
-        print("AutoFilm程序退出！")
+    # try:
+    #     get_event_loop().run_forever()
+    # except (KeyboardInterrupt, SystemExit):
+    #     print("AutoFilm程序退出！")
